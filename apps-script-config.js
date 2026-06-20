217df014609f1e4d28d2cdd6a78583fac91ce8c7
nsosklass88-ctrl <nsosklass88@gmail.com>
2026-06-20 22:02:36 +0900

    Update apps-script-config.js

--- apps-script-config.js
+++ apps-script-config.js
@@ -3,4 +3,4 @@
 // Пример:
 // window.APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec";
 
-window.APPS_SCRIPT_WEB_APP_URL = "";
+window.APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyJsSN1LvmZ1YEOe-P3ywwsqTncVCzq_jx-aAYhGQD3UKEra3qHQv3Tg0hGzroplICwFQ/exec";
